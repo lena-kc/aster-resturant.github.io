28eb862b0b274d2b9258aacd69218ecf14445f7c
lena-kc <linakaci5@gmail.com>
2024-05-09 19:11:06 +0100

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,10 @@
+
+  function scrollToAppetizers() {
+    document.getElementById('container1').style.transform = 'translateX(-0%)';
+  }
+  function scrollToDishes() {
+    document.getElementById('container1').style.transform = 'translateX(-33.33%)';
+  }
+  function scrollToDesserts() {
+    document.getElementById('container1').style.transform = 'translateX(-66.66%)';
+  }
